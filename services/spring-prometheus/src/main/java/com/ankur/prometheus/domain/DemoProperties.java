7f9fd167e7dd13ec1a7570243d1638d34415b7ca
Zell Liew <zellwk@gmail.com>
2022-06-19 16:58:32 +0200

    hello worlds initial commit

---
+++ services/spring-prometheus/src/main/java/com/ankur/prometheus/domain/DemoProperties.java
@@ -0,0 +1,18 @@
+package com.ankur.prometheus.domain;
+
+import java.util.Properties;
+
+public class DemoProperties {
+
+    private Properties runtimeProperties;
+
+    public void setRuntimeProperties(Properties props) {
+        this.runtimeProperties = props;
+    }
+
+    public Properties getRuntimeProperties() {
+        return runtimeProperties;
+    }
+
+    public String getDemoMode() { return this.runtimeProperties.getProperty("demo.mode"); }
+}
---
+++ services/spring-prometheus/src/main/java/com/ankur/prometheus/repository/DeviceRepository.java
@@ -0,0 +1,13 @@
+package com.ankur.prometheus.repository;
+
+import com.ankur.prometheus.domain.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+
+
+}
+
+
